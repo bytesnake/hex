da554c4eaf73ee12d9e27c28f3122b9e5ac56870
Lorenz Schmidt <bytesnake@mailbox.org>
2018-11-10 11:39:54 +0100

    Add actions and search to hex-cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,19 +1,78 @@
 extern crate hex_database;
 extern crate hex_music_container;
-extern crate clap;
+extern crate getopts;
 
 use std::env;
 use std::path::PathBuf;
-use hex_database::Collection;
+use hex_database::{Collection, search::SearchQuery, Track};
+
+use getopts::Options;
 
 fn main() {
     let (path_data, path_db) = env::vars()
         .filter(|(key, _)| key == "HEX_PATH").map(|(_, a)| a).next()
         .map(|x| (PathBuf::from(&x).join("data"), PathBuf::from(&x).join("music.db")))
         .unwrap();
+    let args: Vec<String> = env::args().collect();
 
     let db = Collection::from_file(&path_db);
 
+    // print overview of the database
+    if args.len() == 1 {
+        print_overview(&db);
+        return;
+    }
+
+    // in case the arguments are beginning with a value, we assume a search 
+    let mut search_pattern = args.iter().skip(1)
+        .take_while(|x| !x.contains("-")).cloned()
+        .collect::<Vec<String>>().join(" ");
+
+    // now build the option pattern
+    let mut opts = Options::new();
+    opts.optopt("s", "search", "search for tracks", "QUERY");
+    opts.optopt("a", "action", "execute a certain action", "delete|modify|show|play");
+    opts.optflag("h", "help", "hex command line");
+    let matches = opts.parse(&args[1..]).unwrap();
+
+    if let Some(query) = matches.opt_str("s") {
+        search_pattern = query;
+    }
+
+    let mut action = "show".into();
+    if let Some(new_action) = matches.opt_str("a") {
+        action = new_action;
+    }
+
+    let query = SearchQuery::new(&search_pattern).unwrap();
+    let mut query = db.search_prep(query).unwrap();
+    let tracks: Vec<Track> = db.search(&mut query).collect();
+
+    match action.as_ref() {
+        "show" => {
+            show_tracks(&search_pattern, tracks);
+        },
+        _ => {
+            println!("Unsupported action!");
+            return;
+        }
+    }
+
+}
+
+fn show_tracks(query: &str, tracks: Vec<Track>) {
+    println!("Found {} tracks for query: `{}`", tracks.len(), query);
+    println!("");
+
+    for track in tracks {
+        if let (Some(ref title), Some(ref interpret)) = (&track.title, &track.interpret) {
+            println!("\t{} ## {}", title, interpret);
+        }
+    }
+
+}
+
+fn print_overview(db: &Collection) {
     let mut tracks = db.get_tracks();
     tracks.sort_by(|a, b| a.favs_count.cmp(&b.favs_count).reverse());
 
